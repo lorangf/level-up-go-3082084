76e26dbfefe65c89950196330ac00d4d351fb67e
lorangf <freddy.labete@gmail.com>
2025-01-04 13:01:14 -0500

    fix typo

--- 01_02b/main.go
+++ 01_02b/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"strings"
 	"time"
 )
 
@@ -16,7 +17,16 @@ func print(msg string) {
 // slowDown takes the given string and repeats its characters
 // according to their index in the string.
 func slowDown(msg string) {
-	panic("NOT IMPLEMENTED")
+	words := strings.Split(msg, " ")
+	for _, w := range words {
+		var text []byte
+		for i := 0; i < len(w); i++ {
+			for j := 0; j <= i; j++ {
+				text = append(text, w[i])
+			}
+		}
+		print(string(text))
+	}
 }
 
 func main() {
