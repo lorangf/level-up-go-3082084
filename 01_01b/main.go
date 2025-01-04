517b03a3924c22a5b7328c78ce82f0b2ac32a0d9
Frederic Loranger <frederic.loranger@NOVL-FLORANGER2.local>
2025-01-04 12:01:49 -0500

    example 01_01

--- 01_01b/main.go
+++ 01_01b/main.go
@@ -10,12 +10,18 @@ var expectedFormat = "2006-01-02"
 
 // parseTime validates and parses a given date string.
 func parseTime(target string) time.Time {
-	panic("NOT IMPLEMENTED")
+	bday, err := time.Parse(expectedFormat, target)
+	if err != nil || bday.Before(time.Now()) {
+		log.Fatal("Missing or wrong input dates")
+	}
+	return bday
 }
 
 // calcSleeps returns the number of sleeps until the target.
 func calcSleeps(target time.Time) float64 {
-	panic("NOT IMPLEMENTED")
+	hours := time.Until(target).Hours()
+	days := hours/24 + 1
+	return days
 }
 
 func main() {
