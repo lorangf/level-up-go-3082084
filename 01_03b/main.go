97cf5db80c245b7c326799c745515878938b7f5a
lorangf <freddy.labete@gmail.com>
2025-01-05 08:15:00 -0500

    feat: example 01 03

--- 01_03b/main.go
+++ 01_03b/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -10,18 +12,27 @@ const path = "entries.json"
 
 // raffleEntry is the struct we unmarshal raffle entries into
 type raffleEntry struct {
-	// TODO: Fill in definition
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
 
 // importData reads the raffle entries from file and creates the entries slice.
 func importData() []raffleEntry {
-	panic("NOT IMPLEMENTED")
+	contentBytes, err := os.ReadFile(path)
+	if err != nil {
+		log.Panicf("Could not read the file %v", path)
+	}
+	var entries []raffleEntry
+	if err := json.Unmarshal(contentBytes, &entries); err != nil {
+		log.Panicf("Could not unmarshall the data, %v", err)
+	}
+	return entries
 }
 
 // getWinner returns a random winner from a slice of raffle entries.
 func getWinner(entries []raffleEntry) raffleEntry {
-	rand.Seed(time.Now().Unix())
-	wi := rand.Intn(len(entries))
+	generator := rand.New(rand.NewSource(time.Now().Unix()))
+	wi := generator.Intn(len(entries))
 	return entries[wi]
 }
 
